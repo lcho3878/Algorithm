db955f69372ca51427c97c1f921a3ce102fa3244
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2023-12-12 14:20:13 +0900

    Update 로또의 최고 순위와 최저 순위.swift

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.swift
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.swift
@@ -3,13 +3,9 @@ import Foundation
 func solution(_ lottos:[Int], _ win_nums:[Int]) -> [Int] {
     var zeroCount = 0
     var winningCount = 0
-    for number in lottos {
-        if win_nums.contains(number) {
-            winningCount += 1
-        }
-        else if number == 0 {
-            zeroCount += 1
-        }
+    lottos.forEach {
+        winningCount += win_nums.contains($0) ? 1 : 0
+        zeroCount += $0 == 0 ? 1 : 0
     }
     let lowestLank = winningCount >= 2 ? 7 - winningCount : 6
     let highestLank = lowestLank - zeroCount >= 1 ? lowestLank - zeroCount : 1
