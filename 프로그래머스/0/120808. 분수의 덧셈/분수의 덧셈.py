e7cd0cd885bff3924fdc5e4def612178d2b08600
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2024-03-15 18:36:25 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -0,0 +1,8 @@
+import math
+def solution(numer1, denom1, numer2, denom2):
+    answer = []
+    numer = numer1 * denom2 + numer2 * denom1
+    denom = denom1 * denom2
+    gcd = math.gcd(denom, numer)
+
+    return [numer/gcd, denom/gcd]
