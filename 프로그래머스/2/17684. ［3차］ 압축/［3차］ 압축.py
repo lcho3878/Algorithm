bbc1c57ba473b5ec4e5b033cec29f3dae7e73169
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2024-03-21 19:51:59 +0900

    [level 2] Title: [3차] 압축, Time: 1.63 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -1,18 +1,18 @@
 def solution(msg):
     answer = []
-    dictionary = [chr(i) for i in range(64, 91)]
+    dic = list("@ABCDEFGHIJKLMNOPQRSTUVWXYZ")
     start = 0
     end = 1
     while end <= len(msg):
         now = msg[start:end]
-        while now in dictionary:
+        while now in dic:
             if end > len(msg):
                 break
             end += 1
             now = msg[start:end]
         check = msg[start:end]
         now = msg[start:end-1]
-        answer.append(dictionary.index(now))
-        dictionary.append(check)
+        answer.append(dic.index(now))
+        dic.append(check)
         start += len(now)
     return answer
