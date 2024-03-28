1d58ca2122c739d7f7e824af20f1e886e1482cc2
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2024-03-28 15:13:16 +0900

    [Silver III] Title: 슬라임 합치기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14241. 슬라임 합치기/슬라임 합치기.py
+++ 백준/Silver/14241. 슬라임 합치기/슬라임 합치기.py
@@ -1,7 +1,6 @@
-from collections import deque
 if __name__ == '__main__':
     n = int(input())
-    slimes = deque(sorted(list(map(int, input().split()))))
+    slimes = sorted(list(map(int, input().split())))
     answer = 0
     while len(slimes) > 1:
         x = slimes.pop()
