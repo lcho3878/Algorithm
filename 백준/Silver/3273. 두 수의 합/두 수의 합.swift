2febf45874b0b6a535ae218ad14db7cb0ac14d1c
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2024-10-15 11:26:16 +0900

    [Silver III] Title: 두 수의 합, Time: 44 ms, Memory: 76884 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.swift
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.swift
@@ -1,23 +1,21 @@
-import Foundation
-
 let n = Int(readLine()!)!
 let arr = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
 let x = Int(readLine()!)!
-var start = 0
-var end = arr.count - 1
-var count = 0
-while start < end {
-    let sum = arr[start] + arr[end]
-    if sum == x {
-        count += 1
-        start += 1
-        end -= 1
+var cnt = 0
+var a = 0
+var b = arr.count - 1
+
+while a < b {
+    if arr[a] + arr[b] > x {
+        b -= 1
     }
-    else if sum > x {
-        end -= 1
+    else if arr[a] + arr[b] == x {
+        a += 1
+        b -= 1
+        cnt += 1
     }
     else {
-        start += 1
+        a += 1
     }
 }
-print(count)
+print(cnt)
